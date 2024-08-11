d1874108697afa594f055ef8427c98df93ac451f
marcosscheffer <marcosscheffer2989@gmail.com>
2024-08-11 11:25:30 -0300

    created webhook for message slack

--- app/views/webhook_view.py
+++ app/views/webhook_view.py
@@ -1,16 +1,43 @@
+import os
 from flask_restful import Resource
 from flask import request
-import logging
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+from datetime import datetime
+
 
 from ..extensions import api_v1
 from ..services.leads_service import register_lead_service
-from ..services.seller_service import get_seller_by_email_service
+from ..services.seller_service import get_seller_by_email_service, get_seller_by_id_service
 from ..services.lead_sale_service import register_lead_sale_service
+from ..services.slack_service import send_message_service
 from ..schemas.lead_schema import LeadSchema
 from ..schemas.lead_sale_schema import LeadSaleSchema
 from ..entities.lead import Lead
 from ..entities.lead_sale import LeadSale
 
+def create_message_sold(data):
+    seller_name = get_seller_by_id_service(data.seller_id)
+    date_obj = datetime.strptime(data.sale_date, "%Y-%m-%dT%H:%M:%S")
+    formatted_date = date_obj.strftime("%d/%m/%Y %H:%M:%S")
+    message = f"""
+    ----------------------------------
+    Sold Lead:
+    - ID: {data.id}
+    - Vendedor: {seller_name.first_name} {seller_name.last_name}
+    - Data da Venda: {formatted_date}
+    - Valor: R$ {data.value}
+    - Checklist: {CHECKLIST_URL}
+    """
+    
+    return message
+
+
+load_dotenv()
+SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL')
+CHECKLIST_URL = os.environ.get("CHECKLIST_URL")
+client = WebClient(SLACK_BOT_TOKEN)
 
 class LeadWebhookView(Resource):
     def post(self):
@@ -59,6 +86,13 @@ class LeadSoldWebhookView(Resource):
                         seller_id=seller.id,
                         value=data_lead["value"]
                         )
+        
+        #send sold message to slack channel
+        message = create_message_sold(lead)
+        response_slack = send_message_service(client, SLACK_CHANNEL, message)
+        #send whatsapp url to ts of message sold
+        message_whatsapp_url = "https://wa.me/"
+        send_message_service(client, SLACK_CHANNEL, message_whatsapp_url, response_slack["ts"])
         
         response = register_lead_sale_service(lead)
         return lss.dump(response), 201
