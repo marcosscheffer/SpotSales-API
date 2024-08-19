b993b4685b5d9c4fbe59883c0d669d0380946046
marcosscheffer <marcosscheffer2989@gmail.com>
2024-08-19 14:59:45 -0300

    added script from filter for args

--- app/paginate.py
+++ app/paginate.py
@@ -1,20 +1,42 @@
 from flask import request, url_for
+from sqlalchemy import or_
 
 def paginate(model, schema):
     page = int(request.args.get('page', 1))
     per_page = int(request.args.get('per_page', 10))
-    page_obj = model.query.paginate(page=page, per_page=per_page)
+    q = request.args.get("q", None)
+    
 
+    query = model.query
+
+    if q:
+        filters = []
+        for column in model.__table__.columns:
+            if column.type.python_type == str:
+                filters.append(column.ilike(f"%{q}%"))
+        
+        if filters:
+            query = query.filter(or_(*filters))
+        
+
+    
+        
+    page_obj = query.paginate(page=page, per_page=per_page)
+
+    
+    
     next = url_for(
         request.endpoint,
         page=page_obj.next_num if page_obj.has_next else page_obj.page, 
         per_page=per_page,
+        q=q,
         **request.view_args
     )
     
     prev = url_for(request.endpoint,
                    page=page_obj.prev_num if page_obj.has_prev else page_obj.page, 
                    per_page=per_page,
+                   q=q,
                    **request.view_args)
     
     return {
